a370a5b21624711475960e4beb8446d0b3d3dc9b
saptarshi <saptarshi.majumdar>
2019-11-21 17:13:57 +0530

    fixed api-gateway sonarqube issues

--- api-gateway/src/main/java/com/stackroute/zuulapi/ZuulApiApplication.java
+++ api-gateway/src/main/java/com/stackroute/zuulapi/ZuulApiApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
